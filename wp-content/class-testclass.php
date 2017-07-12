14bf2c09ae7781585fde7ac578b2754eaf5d7324
Cristi Rusu <cristi@crowdfavorite.com>
2017-07-12 14:42:15 +0300

    Commit without phpcs

--- wp-content/class-testclass.php
+++ wp-content/class-testclass.php
@@ -13,7 +13,8 @@ class TestClass {
 	 * Constructor.
 	 */
 	public function __construct() {
-		$a = 1;
+    $a=1;
+    $somethingWrong=1+2;
 		get_category_link($a); // @codingStandardsIgnoreLine WordPress.VIP.RestrictedFunctions
 	}
 }
